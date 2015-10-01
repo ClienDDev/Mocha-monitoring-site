bb69b64b680833e19e15a3a2583d55ed04d9c096
atnartur <atnartur2012@gmail.com>
2015-10-01 20:54:06 +0300

    + API: GET tests

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
-/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.json({status: 'ok'});
+    res.json({status: 'ok'});
+});
+
+router.get('/tests', function(req, res){
+    fs.readdir('./tests/', function(err, files){
+        if(err)
+            res.json({'error': err});
+
+        var return_json = [];
+
+        files.forEach(function(file){
+            if(file != '..' && file != '.')
+                return_json.push(file.replace('.js', ''));
+        });
+
+        res.json(return_json);
+    })
+});
+
+router.get('/tests/:name', function(req, res){
+    res.json(req.params.name);
 });
 
 module.exports = router;
