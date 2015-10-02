ed023b0b1026a8b768132bf37614586fcc7aacae
atnartur <atnartur2012@gmail.com>
2015-10-02 14:54:37 +0300

    + Кнопка перезапуска теста

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@
  * Developer: Artur Atnagulov (atnartur)
  */
 
-var item_dialog_template;
+var item_dialog_template, test_template, test_items_template;
 
 function test_item_click(){
     $('.test_item').unbind('click').click(function(){
@@ -26,6 +26,51 @@ function test_item_click(){
         });
     });
 }
+
+
+function test_init(test){
+    if($('.test[data-name="' + test + '"').length == 0)
+        $('.tests').append(test_template({name: test}));
+
+    var div = $('.test[data-name="' + test + '"');
+
+    div.find('.runtime').html('<i class="fa fa-spin fa-spinner"></i>');
+    div.find('.fail, .pass').html('<i class="fa fa-spin fa-spinner fa-2x"></i>');
+    div.find('.refresh_test').attr('disabled', 'disabled');
+
+    div.find('.refresh_test').unbind('click').click(function(){
+        test_init(test);
+    });
+
+    $.get('/api/tests/' + test, function(res){
+        try {
+            var json = JSON.parse(res);
+        }
+        catch(e){
+            console.error(test, e);
+            div.find('.runtime').text('Ошибка разбора JSON');
+            div.find('.pass, .fail').text('');
+            return;
+        }
+
+        div.find('.runtime').text(json.stats.duration + 'ms');
+        div.find('.pass').html(test_items_template({
+            data: json.passes,
+            success: true
+        }));
+        div.find('.fail').html(test_items_template({
+            data: json.failures,
+            success: false
+        }));
+
+        test_item_click();
+        search_init();
+        div.find('.refresh_test').removeAttr('disabled');
+
+        console.log(json);
+    });
+}
+
 
 function panel_click(){
     $('.panel-heading').unbind('click').click(function(){
@@ -50,45 +95,14 @@ function init(){
     $.get('/js/templates.html', function(html){
         $('body').append(html);
 
-        var test_template = Handlebars.compile($('#test_template').html());
-        var test_items_template = Handlebars.compile($('#test_items_template').html());
+        test_template = Handlebars.compile($('#test_template').html());
+        test_items_template = Handlebars.compile($('#test_items_template').html());
         item_dialog_template = Handlebars.compile($('#item_dialog_template').html());
 
         $.get('/api/tests', function(tests){
             $('.tests').html('');
             tests.forEach(function(test){
-                (function(test){
-                    $('.tests').append(test_template({name: test}));
-
-                    $.get('/api/tests/' + test, function(res){
-                        var div = $('.test[data-name="' + test + '"');
-
-                        try {
-                            var json = JSON.parse(res);
-                        }
-                        catch(e){
-                            console.error(test, e);
-                            div.find('.runtime').text('Ошибка разбора JSON');
-                            div.find('.pass, .fail').text('');
-                            return;
-                        }
-
-                        div.find('.runtime').text(json.stats.duration + 'ms');
-                        div.find('.pass').html(test_items_template({
-                            data: json.passes,
-                            success: true
-                        }));
-                        div.find('.fail').html(test_items_template({
-                            data: json.failures,
-                            success: false
-                        }));
-
-                        test_item_click();
-                        search_init();
-
-                        console.log(json);
-                    });
-                })(test);
+                test_init(test);
             });
 
             panel_click();
